26f746866007abe8f5376ca81657f8a3d0c07ef2
Brett Weir <brett@lamestation.com>
2013-12-27 13:47:45 -0800

    Added terrain brush and reorganized 2bit tilemap for use with tiled; started updating corresponding level

---
+++ tools/maps/tiles_2b_poketron.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_2b_poketron" tilewidth="8" tileheight="8">
+ <image source="tiles_2b_poketron.png" trans="ff00ff" width="104" height="80"/>
+ <terraintypes>
+  <terrain name="Water" tile="-1"/>
+  <terrain name="Grass" tile="-1"/>
+  <terrain name="Castle" tile="-1"/>
+ </terraintypes>
+ <tile id="1" terrain="1,1,1,1"/>
+ <tile id="91" terrain="0,0,0,1"/>
+ <tile id="92" terrain="0,0,1,0"/>
+ <tile id="93" terrain="1,1,1,0"/>
+ <tile id="94" terrain="1,1,0,0"/>
+ <tile id="95" terrain="1,1,0,1"/>
+ <tile id="104" terrain="0,1,0,0"/>
+ <tile id="105" terrain="1,0,0,0"/>
+ <tile id="106" terrain="1,0,1,0"/>
+ <tile id="107" terrain="0,0,0,0"/>
+ <tile id="108" terrain="0,1,0,1"/>
+ <tile id="119" terrain="1,0,1,1"/>
+ <tile id="120" terrain="0,0,1,1"/>
+ <tile id="121" terrain="0,1,1,1"/>
+</tileset>
