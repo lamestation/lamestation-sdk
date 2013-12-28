8f9e3628adf51015a9875e8642cfdf9d01320931
Brett Weir <brett@lamestation.com>
2013-12-27 16:05:29 -0800

    Added new tiles and terrains; worked on planning collision; deleted bitmap versions of tilesets

--- tilestudio/tilesets/README.md
+++
@@ -1,9 +0,0 @@
-Sample Tile Maps
-=================
-
-This folder contains sample graphics that you can use in your games,
-as well as sample maps showing how the pieces fit together.
-
-
-<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />Artwork is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
-
--- tools/maps/tiles_2b_poketron.tsx
+++ tools/maps/tiles_2b_poketron.tsx
@@ -5,8 +5,22 @@
   <terrain name="Water" tile="-1"/>
   <terrain name="Grass" tile="-1"/>
   <terrain name="Castle" tile="-1"/>
+  <terrain name="Trees" tile="-1"/>
  </terraintypes>
  <tile id="1" terrain="1,1,1,1"/>
+ <tile id="5" terrain="1,1,1,3"/>
+ <tile id="6" terrain="1,1,3,3"/>
+ <tile id="7" terrain="1,1,3,1"/>
+ <tile id="18" terrain="1,3,1,3"/>
+ <tile id="19" terrain="3,3,3,3"/>
+ <tile id="20" terrain="3,1,3,1"/>
+ <tile id="31" terrain="1,3,1,1"/>
+ <tile id="32" terrain="3,3,1,1"/>
+ <tile id="33" terrain="3,1,1,1"/>
+ <tile id="44" terrain="3,3,3,1"/>
+ <tile id="45" terrain="3,3,1,3"/>
+ <tile id="57" terrain="3,1,3,3"/>
+ <tile id="58" terrain="1,3,3,3"/>
  <tile id="91" terrain="0,0,0,1"/>
  <tile id="92" terrain="0,0,1,0"/>
  <tile id="93" terrain="1,1,1,0"/>
