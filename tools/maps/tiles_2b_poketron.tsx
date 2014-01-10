48c1b72b8f63332e43719f9e80ce2f9e179a35be
Brett Weir <brett@lamestation.com>
2014-01-10 02:29:54 -0800

    Added tile-based collision mapping to map2dat so that it isn't necessary to added collision tiles in the editor (which made me feel quite crazy)

--- tools/maps/tiles_2b_poketron.tsx
+++ tools/maps/tiles_2b_poketron.tsx
@@ -6,6 +6,7 @@
   <terrain name="Grass" tile="-1"/>
   <terrain name="Castle" tile="-1"/>
   <terrain name="Trees" tile="-1"/>
+  <terrain name="Dirt" tile="-1"/>
  </terraintypes>
  <tile id="1" terrain="1,1,1,1"/>
  <tile id="5" terrain="1,1,1,3"/>
@@ -21,17 +22,30 @@
  <tile id="45" terrain="3,3,1,3"/>
  <tile id="57" terrain="3,1,3,3"/>
  <tile id="58" terrain="1,3,3,3"/>
+ <tile id="71" terrain="4,4,4,1"/>
+ <tile id="72" terrain="4,4,1,4"/>
+ <tile id="84" terrain="4,1,4,4"/>
+ <tile id="85" terrain="1,4,4,4"/>
  <tile id="91" terrain="0,0,0,1"/>
  <tile id="92" terrain="0,0,1,0"/>
  <tile id="93" terrain="1,1,1,0"/>
  <tile id="94" terrain="1,1,0,0"/>
  <tile id="95" terrain="1,1,0,1"/>
+ <tile id="96" terrain="1,1,1,4"/>
+ <tile id="97" terrain="1,1,4,4"/>
+ <tile id="98" terrain="1,1,4,1"/>
  <tile id="104" terrain="0,1,0,0"/>
  <tile id="105" terrain="1,0,0,0"/>
  <tile id="106" terrain="1,0,1,0"/>
  <tile id="107" terrain="0,0,0,0"/>
  <tile id="108" terrain="0,1,0,1"/>
+ <tile id="109" terrain="1,4,1,4"/>
+ <tile id="110" terrain="4,4,4,4"/>
+ <tile id="111" terrain="4,1,4,1"/>
  <tile id="119" terrain="1,0,1,1"/>
  <tile id="120" terrain="0,0,1,1"/>
  <tile id="121" terrain="0,1,1,1"/>
+ <tile id="122" terrain="1,4,1,1"/>
+ <tile id="123" terrain="4,4,1,1"/>
+ <tile id="124" terrain="4,1,1,1"/>
 </tileset>
