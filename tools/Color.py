f4a7750de31b27c17e1f956c35ef8e6f02aa8a47
Brett Weir <brett@lamestation.com>
2014-06-25 01:27:23 -0700

    Forgot to add Color.py

---
+++ tools/Color.py
@@ -0,0 +1,11 @@
+color = {}
+color['plain'] = ["#000000","#7F7F7F","#FFFFFF","#FF00FF"]
+color['white on blue'] = ["#9140FE","#B17DE1","#CCCECB","#000000"]
+color['red on black'] = ["#CC0000","#7F0000","#FF0000","#000000"]
+
+lookup = {  
+        'black':0,
+        'gray':1,
+        'white':2,
+        'none':3
+        }
