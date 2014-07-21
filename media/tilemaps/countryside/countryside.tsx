fca03b8d91f1468fd6603ec415d12df7ffe608fc
Brett Weir <brett@lamestation.com>
2014-07-21 05:01:08 -0700

    Added pikeman tileset; organized media folder, made presentable, and removed old junk

---
+++ media/tilemaps/countryside/countryside.tsx
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="countryside" tilewidth="8" tileheight="8">
+ <image source="countryside.png" trans="ff00ff" width="96" height="80"/>
+ <terraintypes>
+  <terrain name="Grass" tile="-1"/>
+  <terrain name="High Grass" tile="-1"/>
+  <terrain name="Trees" tile="-1"/>
+  <terrain name="Water" tile="-1"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,0,0,2"/>
+ <tile id="6" terrain="0,0,2,2"/>
+ <tile id="7" terrain="0,0,2,0"/>
+ <tile id="17" terrain="0,2,0,2"/>
+ <tile id="18" terrain="2,2,2,2"/>
+ <tile id="19" terrain="2,0,2,0"/>
+ <tile id="29" terrain="0,2,0,0"/>
+ <tile id="30" terrain="2,2,0,0"/>
+ <tile id="31" terrain="2,0,0,0"/>
+ <tile id="41" terrain="2,2,2,0"/>
+ <tile id="42" terrain="2,2,0,2"/>
+ <tile id="53" terrain="2,0,2,2"/>
+ <tile id="54" terrain="0,2,2,2"/>
+ <tile id="66" terrain="1,1,1,0"/>
+ <tile id="67" terrain="1,1,0,1"/>
+ <tile id="78" terrain="1,0,1,1"/>
+ <tile id="79" terrain="0,1,1,1"/>
+ <tile id="84" terrain="3,3,3,0"/>
+ <tile id="85" terrain="3,3,0,3"/>
+ <tile id="86" terrain="0,0,0,3"/>
+ <tile id="87" terrain="0,0,3,3"/>
+ <tile id="88" terrain="0,0,3,0"/>
+ <tile id="89" terrain="0,0,0,1"/>
+ <tile id="90" terrain="0,0,1,1"/>
+ <tile id="91" terrain="0,0,1,0"/>
+ <tile id="96" terrain="3,0,3,3"/>
+ <tile id="97" terrain="0,3,3,3"/>
+ <tile id="98" terrain="0,3,0,3"/>
+ <tile id="99" terrain="3,3,3,3"/>
+ <tile id="100" terrain="3,0,3,0"/>
+ <tile id="101" terrain="0,1,0,1"/>
+ <tile id="102" terrain="1,1,1,1"/>
+ <tile id="103" terrain="1,0,1,0"/>
+ <tile id="110" terrain="0,3,0,0"/>
+ <tile id="111" terrain="3,3,0,0"/>
+ <tile id="112" terrain="3,0,0,0"/>
+ <tile id="113" terrain="0,1,0,0"/>
+ <tile id="114" terrain="1,1,0,0"/>
+ <tile id="115" terrain="1,0,0,0"/>
+</tileset>
