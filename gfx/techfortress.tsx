184ac609343481eb7b1a9a2a2d544b913c74dad0
Brett Weir <brett@lamestation.com>
2015-04-28 01:13:18 -0700

    Start making new levels for piXel

---
+++ gfx/techfortress.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="techfortress" tilewidth="8" tileheight="8">
+ <image source="techfortress.png" trans="ff00ff" width="48" height="56"/>
+</tileset>
