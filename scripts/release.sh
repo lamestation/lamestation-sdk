d2df3506fddf06e24112c1b6df84e90477739feb
Brett Weir <brett@lamestation.com>
2015-09-18 17:30:45 -0700

    Automate SDK Spin file metadata generation and licensing information

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,77 @@
-#!/bin/bash -x
+#!/bin/bash
 set -e
 
 RELEASENAME=lamestation-sdk-$1
+VERSION=$1
 
-mkdir -p .build/
-mv * .build/ -f
-mv .build/scripts .
-mv .build ${RELEASENAME}
+BUILDDIR=.build
+
+rm -rf ./$BUILDDIR/
+mkdir -p $BUILDDIR/
+
+cp * -r $BUILDDIR/
+
+if [ -z $1 ] ; then
+        echo "No version given... pass as parameter"
+fi
+
+echo -n "Packaging SDK"
+while read LINE
+do
+        F=`basename $LINE`
+
+        # Generate file date
+
+        TODAY=`git log $F 2>/dev/null | head -3 | grep "Date" | awk '{print $6}'`
+        CREATED=`git log --reverse $F 2>/dev/null | head -3 | grep "Date" | awk '{print $6}'`
+
+        if [[ -z $TODAY ]] ; then
+                AGE=`date +%Y`
+        elif  [[ ! $CREATED == $TODAY ]] ; then
+                AGE=$CREATED-$TODAY
+        else
+                AGE=$TODAY
+        fi
+
+        mkdir -p $BUILDDIR/`dirname $LINE`
+
+        if [[ ! "$F" =~ ^gfx_.*|^map_.*|^ins_.*|^song_.* ]]; then
+
+                cat > $BUILDDIR/$LINE << EOF
+' ${LINE}
+' -------------------------------------------------
+' Version:   ${VERSION}
+' Author(s): $(git blame $LINE -f \
+  | cut -d'(' -f 2- \
+  | sed -e 's/ [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] .*//g' \
+  | sort \
+  | uniq \
+  | sed ':a;N;$!ba;s/\n/, /g')
+' Copyright (c) $AGE LameStation LLC
+' See end of file for terms of use.
+' -------------------------------------------------
+
+`cat ${LINE}`
+
+
+`cat scripts/license.stub`
+EOF
+        else
+                cat $LINE > $BUILDDIR/$LINE
+
+        fi
+
+        echo -n "."
+
+done < <(git ls-tree -r master --name-only | grep .spin$)
+
+echo "DONE"
+
+rm -rf ./$BUILDDIR/scripts
+
+mv ${BUILDDIR} ${RELEASENAME}
 
 rm -f ${RELEASENAME}.zip
 zip -r ${RELEASENAME}.zip ${RELEASENAME}/
 
-mv ${RELEASENAME} .build
-
-mv .build/* .
-rmdir .build
+mv ${RELEASENAME} ${BUILDDIR} 
